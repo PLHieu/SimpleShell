b35c628307f87e61d61d66e5000f8b948a27372d
LongHieu <hieuflong@gmail.com>
2020-10-29 05:29:58 +0700

    add peusudocode

---
+++ peusudocode.c
@@ -0,0 +1,42 @@
+main(){
+
+    while(){
+        // in ra man hinh ssh>
+
+        // lay input
+        input = layinput()
+
+        if(nhap vao ki tu rong){
+            continue;
+        }
+
+        if(nhap vao ki tu exit){
+            your program will set should run to 0 
+            break;
+        }
+
+        if(nhap vao !!){
+            in ra man hinh cau lenh gan nhat
+            thuc thi cau lenh do (in "\n")
+        }
+
+        if(la lenh > ){
+            xuly>(token[1], token[2]);
+        }else if(la lenh <){
+            xuly<(token[1], token[2]);
+        }else if(la lenh |){
+            xuli|(token[1], token[2]);
+        }else(){
+            thuc thi cau lenh do 
+        }
+
+    }
+
+}
+
+void xuly>(char* token1,char* token2){}
+void xuly<(char* token1,char* token2){}
+void xuly\(char* token1,char* token2){}
+char* layinput(){}
+char** tachtoken(char* input){}
+void chuanhoainput(char*& input){}
