6896455400691fae58b9bc8d73518bac07fddfab
LongHieu <hieuflong@gmail.com>
2020-11-02 15:32:50 +0700

    modify peusudo code

--- peusudocode.c
+++ peusudocode.c
@@ -1,42 +1,61 @@
 main(){
 
-    while(){
+    while(loop){
         // in ra man hinh ssh>
+        printf("osh >");
 
         // lay input
-        input = layinput()
+        input = layinput(); 
 
-        if(nhap vao ki tu rong){
+        if(input == null){
             continue;
         }
 
-        if(nhap vao ki tu exit){
-            your program will set should run to 0 
-            break;
-        }
+        int type=processInput(inputLine,token1,token2, newthread);
+        // tách làm 2 phần .. | > < .....
+        // kiểm tra luôn có & không và lưu vào bool newthread
+        // trả về type 
+        // >:1, <:2, |:3
 
-        if(nhap vao !!){
-            in ra man hinh cau lenh gan nhat
-            thuc thi cau lenh do (in "\n")
-        }
 
-        if(la lenh > ){
-            xuly>(token[1], token[2]);
-        }else if(la lenh <){
-            xuly<(token[1], token[2]);
-        }else if(la lenh |){
-            xuli|(token[1], token[2]);
-        }else(){
-            thuc thi cau lenh do 
-        }
-
+        process(tokenArr1, tokenArr2, type, newthread)
+        // trong này xử lí luôn việc exit        
+    
     }
-
 }
 
-void xuly>(char* token1,char* token2){}
-void xuly<(char* token1,char* token2){}
-void xuly\(char* token1,char* token2){}
-char* layinput(){}
-char** tachtoken(char* input){}
-void chuanhoainput(char*& input){}
+void xuly>(char* token1,char* token2,bool newthread){}
+void xuly<(char* token1,char* token2, bool newthread){}
+void xuly|(char* token1,char* token2, bool newthread){}
+
+
+int type=processInput(char* inputLine,char* token1, char* token2, bool newthread){}
+
+void process(char* token1, char* token2, int type,bool newthread){
+    if (type==1)  { // ">"
+        xuly>(token1,token2, newthread);                  
+    }
+    else if (type==2) // "<"
+    {
+        xuly<(token1,token2, newthread);
+    }
+    else if (type==3) // "|"
+    {
+        xuly|(token1,token2, newthread);   
+    }
+    else
+    {
+    
+        if(token1 == "exits") -> loop = false
+        else if(token1 == "!!"){
+            // thuc thi cau lenh chua trong history
+        }
+        else if(token1 == null){
+            // in ra man hinh thong bao cau lenh rong
+        }
+        else{
+            execArgv(tokenArr1);        
+        }
+    }
+}
+        
